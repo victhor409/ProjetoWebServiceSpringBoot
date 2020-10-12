8fa7c6b0a5332e8a7d7296ccd5b718c00584f6ef
victhor409 <victhor409@gmail,com>
2020-10-12 12:02:25 -0300

    exceptionusers

---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,50 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public User insert(User obj) {
+		repository.save(obj);
+		return obj;
+	}
+	
+	public void delete (Long id) {
+		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updataData(entity,obj);
+		return repository.save(entity);
+	}
+
+	private void updataData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
+	
+}
